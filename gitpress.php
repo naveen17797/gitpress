68b19e4e4789c635e39e1b9b785f394464e4aacf
naveen <kmnaveen101@gmail.com>
2021-05-19 20:29:06 +0530

    Initial commit

---
+++ gitpress.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Gitpress
+Plugin URI: https://github.com/naveen17797/gitpress
+Description: Run your wordpress site on github
+Version: 1.0
+Author: Naveen Muthusamy
+Author URI: http://github.com/naveen17797/
+License: GPL2
+*/
+
+
+
