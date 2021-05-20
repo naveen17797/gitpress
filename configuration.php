69a5e53b84066f7c74dc4d5c41ff33877fc1b15b
naveen <kmnaveen101@gmail.com>
2021-05-20 06:46:50 +0530

    add fields and warning

---
+++ configuration.php
@@ -0,0 +1,89 @@
+<?php
+if ( function_exists( 'acf_add_local_field_group' ) ):
+
+	acf_add_local_field_group( array(
+		'key'                   => 'group_60a5b52b5fe1d',
+		'title'                 => 'Gitpress Configuration',
+		'fields'                => array(
+			array(
+				'key'               => 'field_60a5b540cc9ae',
+				'label'             => 'Github / Gitlab username',
+				'name'              => 'git_username',
+				'type'              => 'text',
+				'instructions'      => 'The username for github / gitlab',
+				'required'          => 1,
+				'conditional_logic' => 0,
+				'wrapper'           => array(
+					'width' => '',
+					'class' => '',
+					'id'    => '',
+				),
+				'default_value'     => '',
+				'placeholder'       => '',
+				'prepend'           => '',
+				'append'            => '',
+				'maxlength'         => '',
+			),
+			array(
+				'key'               => 'field_60a5b564cc9af',
+				'label'             => 'Github / Gitlab password',
+				'name'              => 'git_password',
+				'type'              => 'password',
+				'instructions'      => 'Password for github or gitlab ( dont worry, this data is stored locally, no one can access it except you )',
+				'required'          => 1,
+				'conditional_logic' => 0,
+				'wrapper'           => array(
+					'width' => '',
+					'class' => '',
+					'id'    => '',
+				),
+				'placeholder'       => '',
+				'prepend'           => '',
+				'append'            => '',
+			),
+			array(
+				'key'               => 'field_60a5b5a1cc9b0',
+				'label'             => 'Hosting Site',
+				'name'              => 'hosting_site',
+				'type'              => 'select',
+				'instructions'      => 'The site where you want to host your site',
+				'required'          => 0,
+				'conditional_logic' => 0,
+				'wrapper'           => array(
+					'width' => '',
+					'class' => '',
+					'id'    => '',
+				),
+				'choices'           => array(
+					'github' => 'github',
+					'gitlab' => 'gitlab',
+				),
+				'default_value'     => 'github',
+				'allow_null'        => 0,
+				'multiple'          => 0,
+				'ui'                => 0,
+				'return_format'     => 'value',
+				'ajax'              => 0,
+				'placeholder'       => '',
+			),
+		),
+		'location'              => array(
+			array(
+				array(
+					'param'    => 'user_role',
+					'operator' => '==',
+					'value'    => 'administrator',
+				),
+			),
+		),
+		'menu_order'            => 0,
+		'position'              => 'high',
+		'style'                 => 'default',
+		'label_placement'       => 'top',
+		'instruction_placement' => 'label',
+		'hide_on_screen'        => '',
+		'active'                => true,
+		'description'           => '',
+	) );
+
+endif;
--- gitpress.php
+++ gitpress.php
@@ -9,5 +9,47 @@ Author URI: http://github.com/naveen17797/
 License: GPL2
 */
 
+register_activation_hook(__FILE__, function () {
+	wp_redirect(admin_url("admin.php?page=gitpress-options"));
+});
+
+add_action('init', function () {
+    require_once __DIR__  . '/configuration.php';
+});
+
+
+add_action('admin_notices', function () {
+
+	if ( ! function_exists('get_field')) {
+		?>
+		<div class="notice notice-error">
+			<p><?php echo "ACF needs to be installed for gitpress to work" ?></p>
+		</div>
+		<?php
+        // Dont do next checks if field is not defined.
+        return;
+	}
+
+	if ( ! get_field('git_username', "user_".get_current_user_id()) ) {
+		?>
+		<div class="notice notice-error">
+            <p><?php echo "Gitpress configuration failed : Please set your github / gitlab username " ?> <a href="<?php echo get_edit_profile_url()?>">here</a> </p>
+		</div>
+		<?php
+	}
+
+	if ( ! get_field('git_password', "user_".get_current_user_id())) {
+		?>
+		<div class="notice notice-error">
+			<p><?php echo "Gitpress configuration failed : Please set your github / gitlab password " ?> <a href="<?php echo get_edit_profile_url()?>">here</a></p>
+		</div>
+		<?php
+	}
+
+
+});
+
+
+
 
 
